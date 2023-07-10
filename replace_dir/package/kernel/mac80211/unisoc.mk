a8baf4625a2080ad943e42c6a42c2a5f5e851578
Kazagumo <13860126292@163.com>
2023-07-10 23:43:29 +0000

    Add files via upload

---
+++ replace_dir/package/kernel/mac80211/unisoc.mk
@@ -0,0 +1,21 @@
+PKG_DRIVERS += \
+uwe5622 \
+
+config-$(call config_package,uwe5622) += \
+WLAN_UWE5622 \
+SPRDWL_NG \
+
+config-y += \
+WCN_BSP_DRIVER_BUILDIN \
+AW_WIFI_DEVICE_UWE5622 \
+AW_BIND_VERIFY \
+
+define KernelPackage/uwe5622
+  $(call KernelPackage/mac80211/Default)
+  TITLE:= UWE5622(AW562A) Wireless Driver
+  DEPENDS+= +kmod-mac80211 +kmod-mmc
+  FILES:= \
+  $(PKG_BUILD_DIR)/drivers/net/wireless/uwe5622/unisocwifi/sprdwl_ng.ko \
+  $(PKG_BUILD_DIR)/drivers/net/wireless/uwe5622/unisocwcn/uwe5622_bsp_sdio.ko
+  AUTOLOAD:=$(call AutoProbe,sprdwl_ng)
+endef
